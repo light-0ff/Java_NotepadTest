7f235ee51e1094a02484f03a0a3799201f39be80
light-0ff <tortles777@gmail.com>
2022-03-26 20:52:07 +0200

    Old NetBeans code

--- src/notepadtest23/FXMLController.java
+++ src/notepadtest23/FXMLController.java
@@ -1,26 +1,19 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package notepadtest23;
 
 import java.io.File;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
-import javafx.application.Platform;
+
 import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.TabPane;
 import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyCombination;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import structures.TextTab;
 import tools.DialogManager;
@@ -105,25 +98,6 @@ public class FXMLController implements Initializable {
 
     @FXML
     public void btnHandler(KeyEvent event) {
-//        if (event.isMetaDown()) {
-//            System.out.println("meta is down");
-//            if (event.getCode() == KeyCode.LEFT) {
-//                System.out.println("meta + left");
-//            }
-//        }
-//        if (event.getCode() == KeyCode.LEFT && event.isMetaDown()) {
-//            System.out.println("meta + left");
-//        }
-//        ////////////////////////////////////////////////
-//        if (event.isControlDown()) {
-//            System.out.println("meta is down");
-//            if (event.getCode() == KeyCode.LEFT) {
-//                System.out.println("meta + left");
-//            }
-//        }
-//        if (event.getCode() == KeyCode.LEFT && event.isControlDown()) {
-//            System.out.println("control + left");
-//        }
 
         if (event.getCode() == KeyCode.LEFT && event.isControlDown()) {
             System.out.println("left side");
@@ -131,14 +105,15 @@ public class FXMLController implements Initializable {
         } else if (event.getCode() == KeyCode.RIGHT && event.isControlDown()) {
             System.out.println("right side");
             mn.putToRight();
-
+//            mn.setOldVals(); // вернуть прошлий размер
+        } else if (event.getCode() == KeyCode.S && event.isControlDown()) {
+            ((TextTab) tabpane.getSelectionModel().getSelectedItem()).saveFile();
+        } else if (event.getCode() == KeyCode.N && event.isControlDown()) {
+            tabpane.getTabs().add(new TextTab());
+            tabpane.getSelectionModel().select(tabpane.getTabs().toArray().length-1);
         }
-        /*       else if (event.getCode() == KeyCode.UP && event.isControlDown()) {
-            System.out.println("up side");
-            maximized();
-        }   */
 
-    }
+}
 
     @FXML
     private void fileMenuHnd(Event ev
@@ -148,50 +123,34 @@ public class FXMLController implements Initializable {
                 break;
             case "mClose":
                 break;
-            case "mOpenDir":
-                File[] files1 = DialogManager.openDir(stage);
-                if (files1 != null) {
-                    for (File file : files1) {
-                        TextTab tab = new TextTab(file);
-                        tabpane.getTabs().add(tab);
-                        tabpane.getSelectionModel().select(tab);
-                    }
-                }
-                break;
-
-            case "mOpenAll":
-                List<File> files = DialogManager.openAllFile(stage);
-                if (files != null) {
-                    files.forEach(file -> {
-                        TextTab tab = new TextTab(file);
-                        tabpane.getTabs().add(tab);
-                        tabpane.getSelectionModel().select(tab);
-
-                    });
-                }
-                break;
             case "mOpen":
                 File file = DialogManager.openFile(stage);
                 if (file != null) {
-                    TextTab tab = new TextTab(file);
+                    // check if current tab is empty
+                    //if not, create new tab
 
                     TextTab buffer = (TextTab) tabpane.getSelectionModel().getSelectedItem();
                     if (buffer.getText().equals("New Tab")) {
+                        // если сейчас активна пустая новая вкладка, то заменить ее открытым файлом
                         if (buffer.getArea().getText().equals("")) {
-                            // если сейчас активна пустая новая вкладка, то заменить ее открытым файлом
-//                            tabpane.getSelectionModel().getSelectedItem() = buffer;
-//                            tabpane.getChildrenUnmodifiable().get(buffer.hashCode()).setUserData(new TextTab(file));
+                            ((TextTab) tabpane.getSelectionModel().getSelectedItem()).replace(file);
+                            break;
                         }
                     }
+                    TextTab tab = new TextTab(file);
                     tabpane.getTabs().add(tab);
+                    tabpane.getSelectionModel().select(tabpane.getTabs().toArray().length-1);
                 }
                 break;
             case "mNew":
+                 tabpane.getTabs().add(new TextTab());
                 break;
             case "mSaveAs":
                 break;
             case "mSave":
+                ((TextTab) tabpane.getSelectionModel().getSelectedItem()).saveFile();
                 break;
         }
     }
+
 }
--- src/notepadtest23/NotepadTest23.java
+++ src/notepadtest23/NotepadTest23.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package notepadtest23;
 
 import javafx.application.Application;
@@ -30,7 +25,7 @@ public class NotepadTest23 extends Application {
             //controller
             FXMLController controller = loader.getController();
             controller.setStage(stage);
-            
+
             //scene
             Scene scene = new Scene(root);
             scene.getStylesheets().clear();
